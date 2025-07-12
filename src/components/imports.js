90739ec8a204bb81e5b61c8627457e0f766a6824
Christian <Christianmnaira@gmail.com>
2025-07-12 21:50:09 +0800

    feat: exporting components using one js file

---
+++ src/components/imports.js
@@ -0,0 +1,6 @@
+export {default as Hero} from "./hero/Hero";
+export {default as Navbar} from "./navbar/Navbar";
+export {default as About} from "./about/About";
+export {default as Cta} from "./cta/Cta";
+export {default as Signup} from "./signup/Signup";
+export {default as Login} from "./login/Login";
