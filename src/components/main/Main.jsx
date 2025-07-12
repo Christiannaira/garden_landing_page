ba183953ac9155550505502faa393b605745d4a8
Christian <Christianmnaira@gmail.com>
2025-07-12 22:04:40 +0800

    feat: set up web router

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,11 +1,20 @@
 import "./main.css";
+import { Hero, Login, Signup } from "../imports";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const Main = () => {
-  return (
-    <div>
-      Main
-    </div>
-  )
-}
+   return (
+      <div>
+         <Router>
+            <Routes>
+               <Route path="/" element={<Hero />} />
+               <Route path="/login" element={<Login />} />
+               <Route path="/signup" element={<Signup />} />
 
-export default Main
+            </Routes>
+         </Router>
+      </div>
+   );
+};
+
+export default Main;
