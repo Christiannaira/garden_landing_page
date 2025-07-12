7170e48bc726632142af2ffd2eef1623b49a0723
Christian <Christianmnaira@gmail.com>
2025-07-12 21:58:01 +0800

    feat: initialized navbar component

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,8 +2,20 @@ import "./navbar.css";
 
 const Navbar = () => {
   return (
-    <div>
-      Navbar
+    <div >
+      <section className="navbar" id="navbar">
+        <div className="navbar-content">
+          <div className="navbar-content_image">
+            Logo  
+          </div>
+          <div className="navbar-content_buttons">
+            <button>Login</button>
+            <button>Signup</button>
+          </div>
+        </div>
+
+        <div/>
+      </section>
     </div>
   )
 }
